f1d93e7913ab4a8e3191f5eae6f9b4ce945f03b5
saumitra23 <saumitrashukla23@gmail.com>
2023-06-12 15:46:11 +0530

    basic file and folder setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import ExerciseDetail from "./components/pages/ExerciseDetail";
+import Footer from "./components/Footer";
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Box width="400px" sx={{ width: { xl: "1448px" } }} m="auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+      </Box>
+    </BrowserRouter>
+  );
+};
+
+export default App;
